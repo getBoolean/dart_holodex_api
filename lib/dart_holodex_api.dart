ec8d7680956d6d8f85729da8b61a12c695001926
getBoolean <danielwnewville@gmail.com>
2021-10-01 15:50:51 -0500

    Remove unused imports

--- lib/dart_holodex_api.dart
+++ lib/dart_holodex_api.dart
@@ -3,12 +3,7 @@
 /// More dartdocs go here.
 library dart_holodex_api;
 
-import 'dart:async';
-import 'package:dio/dio.dart';
-import 'src/enums.dart';
-import 'src/models.dart' as models;
-
 export 'src/client.dart';
+export 'src/models.dart';
 export 'src/enums.dart';
-export 'src/response.dart';
 export 'src/exception.dart';
