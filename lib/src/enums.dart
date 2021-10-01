ee4086922b84fe6d31e499721280f1b9a6f0ff55
getBoolean <danielwnewville@gmail.com>
2021-10-01 16:29:18 -0500

    Update language code

--- lib/src/enums.dart
+++ lib/src/enums.dart
@@ -18,14 +18,15 @@ enum ChannelType { vtuber, subber }
 // Below classes are based off of https://github.com/EBro912/Holodex.NET
 
 class Language {
-  static const String english = "en";
-  static const String japanese = "jp";
-  static const String spanish = "es";
-  static const String chinese = "zh";
-  static const String korean = "ko";
-  static const String french = "fr";
-  static const String indonesian = "id";
-  static const String russian = "ru";
+  static const String all = 'all';
+  static const String english = 'en';
+  static const String japanese = 'jp';
+  static const String spanish = 'es';
+  static const String chinese = 'zh';
+  static const String korean = 'ko';
+  static const String french = 'fr';
+  static const String indonesian = 'id';
+  static const String russian = 'ru';
 }
 
 
@@ -45,33 +46,33 @@ class VideoSearchType {
 enum SortOrder { ascending, descending }
 
 class Organization {
-  static const String inc774 = "774inc";
-  static const String AogiriHighschool = "Aogiri%20Highschool";
-  static const String AtelierLive = "Atelier%20Live";
-  static const String Chukorara = "Chukorara";
-  static const String EileneFamily = "Eilene%20Family";
-  static const String HanayoriJoshiryo = "Hanayori%20Joshiryo";
-  static const String Hololive = "Hololive";
-  static const String Independent = "Independents";
-  static const String Iridori = "Iridori";
-  static const String KizunaAI = "Kizuna%20Ai%20Inc.";
-  static const String dotLIVE = ".LIVE";
-  static const String Marbl_s = "Marbl_s";
-  static const String Masquerade = "Masquerade";
-  static const String Nijisanji = "Nijisanji";
-  static const String NoriPro = "Nori%20Pro";
-  static const String PRISM = "PRISM";
-  static const String ReACT = "ReACT";
-  static const String RiotMusic = "Riot%20Music";
-  static const String Tsunderia = "Tsunderia";
-  static const String VDimensionCreators = "V%20Dimension.Creators";
-  static const String VirtualWitchPhenomenon = "Virtual%20Witch%20Phenomenon";
-  static const String ViViD = "ViViD";
-  static const String VOMS = "VOMS";
-  static const String VShojo = "VShojo";
-  static const String VSpo = "VSpo";
+  static const String inc774 = '774inc';
+  static const String AogiriHighschool = 'Aogiri%20Highschool';
+  static const String AtelierLive = 'Atelier%20Live';
+  static const String Chukorara = 'Chukorara';
+  static const String EileneFamily = 'Eilene%20Family';
+  static const String HanayoriJoshiryo = 'Hanayori%20Joshiryo';
+  static const String Hololive = 'Hololive';
+  static const String Independent = 'Independents';
+  static const String Iridori = 'Iridori';
+  static const String KizunaAI = 'Kizuna%20Ai%20Inc.';
+  static const String dotLIVE = '.LIVE';
+  static const String Marbl_s = 'Marbl_s';
+  static const String Masquerade = 'Masquerade';
+  static const String Nijisanji = 'Nijisanji';
+  static const String NoriPro = 'Nori%20Pro';
+  static const String PRISM = 'PRISM';
+  static const String ReACT = 'ReACT';
+  static const String RiotMusic = 'Riot%20Music';
+  static const String Tsunderia = 'Tsunderia';
+  static const String VDimensionCreators = 'V%20Dimension.Creators';
+  static const String VirtualWitchPhenomenon = 'Virtual%20Witch%20Phenomenon';
+  static const String ViViD = 'ViViD';
+  static const String VOMS = 'VOMS';
+  static const String VShojo = 'VShojo';
+  static const String VSpo = 'VSpo';
   static const String Xencount = "X%20enc'ount";
-  static const String YuniCreate = "YuniCreate";
+  static const String YuniCreate = 'YuniCreate';
 }
 
 /// A class which contains strings that allow extra data to be returned when requesting videos.
