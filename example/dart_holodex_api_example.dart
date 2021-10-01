09e157cae4ece5ed3ed049fbb086ee1c72399a60
getBoolean <danielwnewville@gmail.com>
2021-10-01 12:45:40 -0500

    Update example

--- example/dart_holodex_api_example.dart
+++ example/dart_holodex_api_example.dart
@@ -2,6 +2,6 @@ import 'package:dart_holodex_api/dart_holodex_api.dart';
 
 void main() async {
   var client = HolodexClient(apiKey: '');
-  final video = await client.getVideo(videoId: '64f654dsf3sd5f');
+  final video = await client.getVideo('64f654dsf3sd5f');
   print(video.toString());
 }
