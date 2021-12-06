f0c38c507c6701b5cccda9af1cac60a648f2e380
getBoolean <danielwnewville@gmail.com>
2021-12-06 13:48:14 -0600

    Add test Test HolodexClient.getVideoFromId()

--- test/dart_holodex_api_test.dart
+++ test/dart_holodex_api_test.dart
@@ -42,7 +42,66 @@ void main() {
     test('API key should not be empty', () async {
       final video = await client.getVideoFromId('Gx_GPwpyLxw');
       print(video);
-      expect(video.title, isNotEmpty);
+
+      expect(
+        video.type,
+        VideoType.clip,
+        reason: 'Expected video to be a clip',
+      );
+
+      expect(
+        video.duration,
+        205,
+        reason: 'Expected video duration to be 205 seconds',
+      );
+
+      expect(
+        video.status,
+        VideoStatus.past,
+        reason: 'Expected video status to be in the past',
+      );
+
+      expect(
+        video.startScheduled,
+        isNull,
+        reason: 'Video is not a premier or a live stream',
+      );
+
+      expect(
+        video.startActual,
+        isNull,
+        reason: 'Video is not a premier or a live stream',
+      );
+
+      expect(
+        video.endActual,
+        isNull,
+        reason: 'Video is not a premier or a live stream',
+      );
+
+      expect(
+        video.liveViewers,
+        isNull,
+        reason: 'Video is not a premier or a live stream',
+      );
+
+      expect(
+        video.songcount,
+        isNull,
+        reason: 'Video has no songs',
+      );
+
+      expect(
+        video.songs,
+        isEmpty,
+        reason: 'Video has no songs',
+      );
+
+      expect(
+        video.language,
+        'en',
+        reason: 'Video language is english',
+      );
     });
   });
 
