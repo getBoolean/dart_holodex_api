8f8631fb268d313df8d61c694d0589fca52712ac
getBoolean <danielwnewville@gmail.com>
2021-12-06 13:38:11 -0600

    Add setup and teardown to tests

--- test/dart_holodex_api_test.dart
+++ test/dart_holodex_api_test.dart
@@ -7,30 +7,44 @@ void main() {
   // I recommend using envify for a production app, this way is just simpler for an example app
   load();
 
-  // Create client with API key from a .env file in the root package directory
-  // See this page on getting an api key https://holodex.stoplight.io/docs/holodex/ZG9jOjQ2Nzk1-getting-started
-  // Add the api key to the .env file in the format `API=api_key` with `api_key` being the key you got from the above website
-  if (!isEveryDefined(['API'])) {
-    print('API key not provided, can not run tests');
-    return;
-  }
+  late String apiKey;
+  late HolodexClient client;
+  setUp(() {
+    // Create client with API key from a .env file in the root package directory
+    // See this page on getting an api key https://holodex.stoplight.io/docs/holodex/ZG9jOjQ2Nzk1-getting-started
+    // Add the api key to the .env file in the format `API=api_key` with `api_key` being the key you got from the above website
+    if (!isEveryDefined(['API'])) {
+      print('API key not provided, can not run tests');
+      return;
+    }
 
-  final String? apiKey = env['API'];
-  if (apiKey == null) {
-    print('API key not provided, can not run tests');
-    return;
-  }
+    final String? _apiKey = env['API'];
+    if (_apiKey == null) {
+      print('API key not provided, can not run tests');
+      return;
+    }
+    apiKey = _apiKey;
 
-  final client = HolodexClient(apiKey: apiKey);
+    client = HolodexClient(apiKey: apiKey);
+  });
+
   group('Test API Key', () {
-    setUp(() {
-      // Additional setup goes here.
-    });
+    setUp(() {});
 
     test('API key should not be empty', () {
       expect(client.apiKey, isNotEmpty);
     });
   });
 
-  client.close();
+  group('Test HolodexClient.getVideoFromId()', () {
+    setUp(() {});
+
+    test('API key should not be empty', () async {
+      final video = await client.getVideoFromId('Gx_GPwpyLxw');
+      print(video);
+      expect(video.title, isNotEmpty);
+    });
+  });
+
+  tearDown(() => client.close());
 }
