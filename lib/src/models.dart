8f81367f4b06cf29371298b78d3dbf96e6d73f10
getBoolean <getboolean@gmail.com>
2023-01-29 00:31:40 -0600

    Removed files no longer used

--- lib/src/models.dart
+++ lib/src/models.dart
@@ -7,8 +7,6 @@ export 'models/api/video_full.dart';
 export 'models/api/comment.dart';
 export 'models/api/song.dart';
 export 'models/paginated_videos.dart';
-export 'models/serializable.dart';
 export 'models/video_filter.dart';
 export 'models/channel_filter.dart';
 export 'models/search_filter.dart';
-export 'models/channel_stats.dart';
