724345aea8ece0cce3dbead5595e0d9628642921
getBoolean <getboolean@gmail.com>
2023-01-30 01:50:17 -0600

    simplify impl

--- lib/src/holodex_client.dart
+++ lib/src/holodex_client.dart
@@ -80,46 +80,13 @@ class HolodexClient {
       paginated: false,
     ),
   ]) async {
-    final languages =
-        filter.languages.isEmpty ? [Language.all] : filter.languages;
-
     // Create the params list
-    final Map<String, dynamic> params = {};
-
-    // Add the items with default values (they can't be null)
-    params.addAll({
-      'limit': '${filter.limit}',
-      'offset': '${filter.offset}',
-      'order': filter.order.code,
-    });
+    final Map<String, dynamic> params = filter.toJson();
 
     _addVideoSort(filter.sort, params);
 
-    _addPaginated(filter.paginated, params);
-
-    _addChannelId(filter.channelId, params);
-
-    _addId(filter.ids, params);
-
-    // Add the info the videos must include
-    final includes = filter.includes;
-    _addIncludes(includes, params);
-
-    // Add the languages to filter by
-    // Add the first item so that there is not a comma in front
-    _addLanguages(languages, params);
-
-    // Add the max upcoming hours param
-    _addMaxUpcomingHours(filter.maxUpcomingHours, params);
-
-    // Add the mentioned channel id param
-    _addMentionedChannelId(filter.mentionedChannelId, params);
-
     // Add the organization param
     _addOrganizations(filter.organization, params);
-
-    // Add the topic param
-    _addTopic(filter.topic, params);
 
     // Add the status param
     _addStatusList(filter.status, params);
