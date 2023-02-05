815f19441c1ba31dc7d46adee32b08a7203959fa
getBoolean <getboolean@gmail.com>
2023-02-05 02:54:45 -0600

    Create search_videos_test.dart

---
+++ test/holodex_client/search_videos_test.dart
@@ -0,0 +1,48 @@
+import 'package:dart_holodex_api/dart_holodex_api.dart';
+import 'package:test/test.dart';
+
+import '../utils.dart';
+
+void main() {
+  late HolodexClient client;
+  setUp(() {
+    client = setUpHolodexClient();
+  });
+  tearDown(() => client.close());
+
+  group('HolodexClient.searchVideos', () {
+    test('Getting live videos paginated', () async {
+      // Setup
+      final filter = const SearchFilter(
+        sort: SearchSort.newest,
+        paginated: true,
+        offset: 0,
+        limit: 5,
+      );
+
+      // Test
+      final videos = await client.searchVideos(filter: filter);
+
+      // Expect
+      expect(videos.total, isNotNull,
+          reason: 'Total should be included if paginated');
+    });
+
+    test('Getting live videos not paginated', () async {
+      // Setup
+      final filter = const SearchFilter(
+        sort: SearchSort.newest,
+        paginated: false,
+        offset: 0,
+        limit: 5,
+      );
+
+      // Test
+      final videos = await client.searchVideos(filter: filter);
+
+      // Expect
+      expect(videos.total, isNull,
+          reason: 'Total should not be included if not paginated');
+    });
+  });
+}
